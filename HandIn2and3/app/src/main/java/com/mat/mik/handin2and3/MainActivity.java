577ff15994ebd91f0539d9026f21ff0a8ef2cc1b
mat-hapahapa <matry7@gmail.com>
2016-05-03 21:57:15 +0200

    Ikoner til vejret tilføjet vha. gradle

--- HandIn2and3/app/src/main/java/com/mat/mik/handin2and3/MainActivity.java
+++ HandIn2and3/app/src/main/java/com/mat/mik/handin2and3/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mat.mik.handin2and3;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.github.pwittchen.weathericonview.WeatherIconView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: Whole assignment
+        // TODO: Populate ListView
+
+        /* Change icon to match current weather - from project: https://github.com/pwittchen/WeatherIconView */
+        WeatherIconView weatherIconView;
+        weatherIconView = (WeatherIconView) findViewById(R.id.weatherImg);
+        //weatherIconView.setIconResource(getString(R.string.wi_day_sunny_overcast));
+        weatherIconView.setIconResource(getString(R.string.wi_day_cloudy));
+        weatherIconView.setIconSize(100);
+        weatherIconView.setIconColor(Color.BLACK);
+
     }
 }
