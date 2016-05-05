8bc5b4ede11703f088a9ef9c92afe58077303fc3
Mikkel Koch Jensen <im.mikkel@gmail.com>
2016-05-05 15:06:25 +0200

    Databasehelper almost done!

--- HandIn2and3/app/src/main/java/com/mat/mik/handin2and3/Managers/DatabaseManager.java
+++ HandIn2and3/app/src/main/java/com/mat/mik/handin2and3/Managers/DatabaseManager.java
@@ -1,13 +1,28 @@
 package com.mat.mik.handin2and3.Managers;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
+import com.mat.mik.handin2and3.Models.WeatherInfo;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
 /**
- * Created by Mikkel on 05-05-2016.
+ * Created by Mikkel on 05-05-2016. a
  */
+
+/**
+ * This class was heavily inspired by the examples from this link
+ * http://developer.android.com/training/basics/data-storage/databases.html
+ * Therefore similarities might occur.
+ */
+
 public class DatabaseManager extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
@@ -31,12 +46,69 @@ public class DatabaseManager extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(SQL_CREATE_ENTRIES);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
 
+    public Long addWeatherInfo(WeatherInfo weatherInfo) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(Contract.WeatherInfoEntry.COLUMN_DESCRIPTION, weatherInfo.description);
+        values.put(Contract.WeatherInfoEntry.COLUMN_TEMPERATURE, weatherInfo.temperature);
+        values.put(Contract.WeatherInfoEntry.COLUMN_TIMESTAMP, weatherInfo.timestamp.toString());
+
+        long id = db.insert(Contract.WeatherInfoEntry.TABLE_NAME, null, values);
+        if(id == -1) {
+            //TODO: Errorhandling
+        }else
+        {
+            weatherInfo.id = id;
+        }
+        return id;
+    }
+
+    public ArrayList<WeatherInfo> getWeatherInfo(){
+
+        SQLiteDatabase db = getReadableDatabase();
+        // Define a projection that specifies which columns from the database
+// you will actually use after this query.
+        String[] projection = {
+                Contract.WeatherInfoEntry.COLUMN_ID,
+                Contract.WeatherInfoEntry.COLUMN_DESCRIPTION,
+                Contract.WeatherInfoEntry.COLUMN_TEMPERATURE,
+                Contract.WeatherInfoEntry.COLUMN_TIMESTAMP
+        };
+
+// How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                Contract.WeatherInfoEntry.COLUMN_ID + " DESC";
+
+        Cursor c = db.query(
+                Contract.WeatherInfoEntry.TABLE_NAME,  // The table to query
+                projection,                               // The columns to return
+                null,                                // The columns for the WHERE clause
+                null,                            // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
+        c.moveToFirst();
+        ArrayList<WeatherInfo> weatherInfoList = new ArrayList<>();
+        while(c.moveToNext()) {
+            WeatherInfo info = new WeatherInfo();
+
+            String timestampString = c.getString(c.getColumnIndex(
+                    Contract.WeatherInfoEntry.COLUMN_TIMESTAMP));
+            DateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+
+            weatherInfoList.add(info);
+        }
+        return weatherInfoList;
     }
 
     public final class Contract {
