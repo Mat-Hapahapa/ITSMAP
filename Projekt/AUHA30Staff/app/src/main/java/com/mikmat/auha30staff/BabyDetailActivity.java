b2327a6aa40815572f12755d367c95d83503366e
Mikkel Koch Jensen <im.mikkel@gmail.com>
2016-05-19 15:45:39 +0200

    added shit

--- Projekt/AUHA30Staff/app/src/main/java/com/mikmat/auha30staff/BabyDetailActivity.java
+++ Projekt/AUHA30Staff/app/src/main/java/com/mikmat/auha30staff/BabyDetailActivity.java
@@ -21,10 +21,14 @@ public class BabyDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_baby_detail);
 
         mTextViewName = (TextView) findViewById(R.id.text_name);
+        mTextViewBirthday = (TextView) findViewById(R.id.text_birthday);
+        mTextviewGender = (TextView) findViewById(R.id.text_gender);
 
         Intent intent = getIntent();
         mBaby = (Baby) intent.getSerializableExtra("barn");
 
         mTextViewName.setText(mBaby.getName());
+        mTextViewBirthday.setText(mBaby.getBirthday().toString());
+        mTextviewGender.setText(mBaby.getGender());
     }
 }
