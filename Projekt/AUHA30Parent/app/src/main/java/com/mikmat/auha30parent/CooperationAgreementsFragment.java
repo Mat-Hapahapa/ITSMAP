3751ce57757340eec40d50ce529422c6da3cf306
Mikkel Koch Jensen <im.mikkel@gmail.com>
2016-05-25 14:08:23 +0200

    Added listview to cooperation agreement fragment.

--- Projekt/AUHA30Parent/app/src/main/java/com/mikmat/auha30parent/CooperationAgreementsFragment.java
+++ Projekt/AUHA30Parent/app/src/main/java/com/mikmat/auha30parent/CooperationAgreementsFragment.java
@@ -7,14 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
 
-public class CooperationAgreementsFragment extends Fragment {
+public class CooperationAgreementsFragment extends Fragment implements AdapterView.OnItemClickListener {
 
     private static String[] mAgreementList;
     private FragmentInteractionListener mListener;
@@ -41,7 +44,15 @@ public class CooperationAgreementsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_cooperation_agreements, container, false);
         mAgreementList = getContext().getResources().getStringArray(R.array.array_agreements);
+
+        mListViewAgreements = (ListView) rootView.findViewById(R.id.list_agreements);
+        mListViewAgreements.setOnItemClickListener(this);
+
+        AgreementAdapter adapter = new AgreementAdapter(inflater);
+        mListViewAgreements.setAdapter(adapter);
+        return rootView;
     }
 
 
@@ -61,6 +72,43 @@ public class CooperationAgreementsFragment extends Fragment {
         super.onDetach();
         mListener = null;
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    private class AgreementAdapter extends BaseAdapter {
+        LayoutInflater inflater;
+
+        public AgreementAdapter(LayoutInflater inflater) {
+            this.inflater = inflater;
+        }
+
+        @Override
+        public int getCount() {
+            return mAgreementList.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mAgreementList[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if(convertView == null) {
+                convertView = inflater.inflate(android.R.layout.simple_list_item_1, null);
+                TextView textView = (TextView) convertView.findViewById(android.R.id.text1);
+                textView.setText((String)getItem(position));
+            }
+            return convertView;
+        }
+    }
 }
-
-
